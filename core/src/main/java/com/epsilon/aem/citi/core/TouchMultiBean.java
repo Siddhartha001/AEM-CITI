ed0d42509397f49a6f2f73cf60dfe8b9941f1d94
sdantani <sdantani@IN-LT70861.corp.epsilon.com>
2018-05-03 19:19:53 +0530

    5/3 - updates done for catalog component

---
+++ core/src/main/java/com/epsilon/aem/citi/core/TouchMultiBean.java
@@ -0,0 +1,33 @@
+package com.epsilon.aem.citi.core;
+ 
+public class TouchMultiBean {
+ 
+     
+//A Data Mamber for Each field in the Coral Multi-field    
+private String product;
+private String link;
+private String image;
+ 
+	public String getImage() {
+	return image;
+	}
+	  
+	public void setImage(String image) {
+	this.image = image;
+	}
+	  	 
+	public String getProduct() {
+	return product;
+	}public void setProduct(String product) {
+	this.product = product;
+	}
+	
+	public String getLink() {
+	return link;
+	}
+	
+	public void setLink(String link) {
+	this.link = link;
+	}
+ 
+}
